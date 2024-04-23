21eaa2999380804705c03ccf5d9f9cb709b01d3c
tl <tommi.latti@netum.fi>
2024-04-23 09:36:16 +0300

    add custom field

--- log.go
+++ log.go
@@ -3,6 +3,8 @@ package rlog
 import (
     "fmt"
     "os"
+    "sync"
+
     "github.com/sirupsen/logrus"
     nested "github.com/cyberys/nested-logrus-formatter"
 )
@@ -11,20 +13,21 @@ var (
     log *logrus.Logger
     f *os.File
     err error
+    queueNumber int
+    queueNumberSet sync.Once
 )
 
 func init() {
-
     logfile := os.Getenv("LOGFILE")
     if logfile == "" {
         f = os.Stdout
     } else {
-        f, err = os.OpenFile(logfile, os.O_APPEND | os.O_CREATE | os.O_RDWR, 0600)
+        f, err = os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+        if err != nil {
+            fmt.Printf("Error opening log file %s, defaulting to standard output: %v\n", logfile, err)
+            f = os.Stdout
+        }
     }
-    if err != nil {
-        fmt.Printf("error opening file: %v", err)
-    }
-//    defer f.Close()
 
     lvl, ok := os.LookupEnv("RLOG_LEVEL")
     if !ok {
@@ -34,42 +37,47 @@ func init() {
     if err != nil {
         ll = logrus.DebugLevel
     }
+
     log = &logrus.Logger{
         Out: f,
         Formatter: &nested.Formatter{
             TimestampFormat: "2006-01-02 15:04:05",
-            HideKeys:         true,
-            NoColors:         true,
-            ShowFullLevel:    true,
+            HideKeys:        true,
+            NoColors:        true,
+            ShowFullLevel:   true,
         },
         Level: ll,
     }
-    log.SetOutput(f)
+}
 
+func SetQueueNumber(num int) {
+    queueNumberSet.Do(func() {
+        queueNumber = num
+    })
 }
 
 func Info(msg string, vars ...interface{}) {
-    log.Infof(msg, vars...)
+    log.WithField("queue", queueNumber).Infof(msg, vars...)
 }
 
 func Debug(msg string, vars ...interface{}) {
-    log.Debugf(msg, vars...)
+    log.WithField("queue", queueNumber).Debugf(msg, vars...)
 }
 
 func Error(msg string, vars ...interface{}) {
-    log.Errorf(msg, vars...)
+    log.WithField("queue", queueNumber).Errorf(msg, vars...)
 }
 
 func Trace(msg string, vars ...interface{}) {
-    log.Tracef(msg, vars...)
+    log.WithField("queue", queueNumber).Tracef(msg, vars...)
 }
 
 func Warn(msg string, vars ...interface{}) {
-    log.Warningf(msg, vars...)
+    log.WithField("queue", queueNumber).Warningf(msg, vars...)
 }
 
 func Fatal(msg string, vars ...interface{}) {
-    log.Fatalf(msg, vars...)
+    log.WithField("queue", queueNumber).Fatalf(msg, vars...)
 }
 
 func Level() string {
